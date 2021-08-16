a8fff3497c16c39990a01a4e799552581af03c01
Jaksa Vlahovic <jaksa.vlahovic1@.gmail.com>
2021-08-16 16:33:14 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { SpeechProvider } from '@speechly/react-client'
+
+import App from './App'
+import './index.css'
+import { Provider } from './context/context'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <SpeechProvider appId="c8fac48b-0201-4ec5-a312-417399a973ba" language="en-US">
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
